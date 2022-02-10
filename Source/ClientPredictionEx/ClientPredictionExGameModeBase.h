dc1f964fd84ac2f325a2bafd6302f4c7e40c1f65
Logan Pazol <logan2299@yahoo.com>
2022-02-09 19:13:11 -0500

    Initial commit

---
+++ Source/ClientPredictionEx/ClientPredictionExGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClientPredictionExGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLIENTPREDICTIONEX_API AClientPredictionExGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
